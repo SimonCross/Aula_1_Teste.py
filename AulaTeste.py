38d68953ea2edd9672a2d08d545a706fcbe26ab9
SimonCross <92488218+SimonCross@users.noreply.github.com>
2023-12-14 22:08:06 -0300

    Add files via upload

---
+++ AulaTeste.py
@@ -0,0 +1,3 @@
+print("Olá mundo")
+print("Este é meu primeiro programa a ser publicado na internet \n espero que possa ser o primeiro de muitos, não\n"
+      "tenho muita experiencia mas almejo grandes alturas \n tendo metas desafiadoras porem realistas")
